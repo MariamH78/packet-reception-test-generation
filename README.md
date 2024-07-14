845c69183e443b1650578f00683c85912a508ed1
Mariam Hesham <99722575+MariamH78@users.noreply.github.com>
2024-07-14 04:34:20 +0300

    Added readme, slightly modified main.cpp

---
+++ README.md
@@ -0,0 +1,45 @@
+
+# packet-reception-test-generation:
+Assume there is a reader instance that receives N packets from M modules i.e. module1, module2 … moduleM.
+
+The reader instance reads from the modules in a ring-like manner, if first packet was received from module N, the next packet can only be from either module N or module N + 1, and if there’s no module N + 1, then it can be received from 1 as well. So, effectively, the only allowed senders after the first packet are module<sub>i</sub> and module<sub>i % M + 1</sub>, where i is the previous packet’s module, and M is the number of modules. Packets received that do not comply to these rules are considered invalid.
+
+The file above generates all possible scenarios of order of receipt for some given packets, where each packet has an associated module number.
+
+## How to run locally:
+
+1. Clone this repository
+```sh
+git clone https://github.com/MariamH78/test-generation.git 
+```
+2. Build main.cpp (The command below assumes g++ exists on the machine)
+```sh
+g++ -o run main.cpp
+```
+3. Run the resulting file, and make sure to include the input file as an argument, for example:
+```sh
+run sample_input.txt
+```
+
+## Input formatting
+
+The file with the input as specified in the requirements. The input must be written on one line (any other lines are ignored), with the first number being the number of packets, and the remaining numbers are the corresponding module numbers for each packet.
+
+## Output format:
+
+The program output requires all scenarios to be generated, so a sequence of all scenarios will be formatted like the given sample, where each scenario will consist of the sequence of rows with PacketIDs from 1 to N.
+
+For the sample input, the output file looks like:
+
+PacketID | ModuleNumber  | ValidModule | 
+--- | --- | --- | 
+1|1|Yes|
+2|3|No|
+3|4|Yes|
+4|4|Yes|
+1|1|Yes|
+..|..|..|
+1|4|Yes|
+2|4|Yes|
+3|3|No|
+4|1|No|
--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@
 using namespace std;
 
 
+string getInput(int argc, char *argv[]);
 vector<int> validate(string userInput);
 string getFileName();
-string getInput(int argc, char *argv[]);
+string writeOutput(int& n, vector<int>& packets);
 
 int main(int argc, char *argv[]) {
-
     string userInput = getInput(argc, argv);
     vector<int> validated = validate(userInput);
 
@@ -25,9 +25,74 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i <= n; i++)
         packets[i - 1] = validated[i];
 
+    string fileName = writeOutput(n, packets);
+    cout << "The file " << fileName << " was created successfully.\n";
+    return 0;
+}
+
+
+vector<int> validate(string userInput){
+    string token = "";
+    vector<int> validated;
+
+    // To make it easier to parse the final token
+    userInput += ' ';
+
+    for (char c : userInput){
+        if (c == ' '){
+            // If a token was all zeros, it's ignored.
+            if (token.empty())
+                continue;
+
+            // If a token has more than 5 digits, it's deemed too large.
+            if (token.size() > 5)
+                throw runtime_error("The number " + token + " is too large.");
+
+            // Otherwise, it's a valid number.
+            validated.push_back(stoi(token));
+            token = "";
+
+        } else {
+            if (!isdigit(c))
+                throw runtime_error("The character '" + string(1, c) + "' was found in the input.");
+
+            // Leading zeros in numbers are ignored.
+            if (token.empty() && c == '0') continue;
+            token += c;
+        }
+    }
+
+    // The first token represents the number of packets,
+    // so if the number doesn't match, that's an error.
+    if (validated.empty() || validated.size() != validated[0] + 1 || (validated[0] == 0)){
+        throw runtime_error("The number of packet module numbers does not match"
+                            " the number of packets (first number in the input),"
+                            " or the input is empty.");
+    }
+
+    for (int i = 1; i <= validated[0]; i++){
+        if (validated[i] > validated[0]) {
+            throw runtime_error("One of the packets has a module number that is larger"
+                                " than N (first number in the input)");
+        }
+    }
+
+    if (validated[0] > 10){
+        cout << "The number of modules is high, which means the program might be"
+                " slow, and might use a lot of memory. Press any key to confirm and"
+                " proceed, or cancel the operation.";
+        char dummy;
+        cin >> dummy;
+    }
+
+    return validated;
+}
+
+string writeOutput(int& n, vector<int>& packets){
     // Create the file we'll write to
-    ofstream csv_file(getFileName());
-    csv_file << "PacketID,MoudleNumber,ValidModule\n";
+    string fileName = getFileName();
+    ofstream csv_file(fileName);
+    csv_file << "PacketID,ModuleNumber,ValidModule\n";
 
     // Sort so that next_permutation() works properly
     sort(packets.begin(), packets.end());
@@ -48,56 +113,7 @@ int main(int argc, char *argv[]) {
     } while (next_permutation(packets.begin(), packets.end()));
 
     csv_file.close();
-
-    return 0;
-}
-
-
-vector<int> validate(string userInput){
-    string token = "";
-    vector<int> validated;
-
-    // To make it easier to parse the final token
-    userInput += ' ';
-
-    for (char c : userInput){
-        if (c == ' '){
-            // If a token was all zeros, it's ignored.
-            if (token.empty())
-                continue;
-
-            // If a token has more than 5 digits, it's deemed too large.
-            if (token.size() > 5) {
-                throw runtime_error("The number " + token + " is too large.");
-                return {};
-            }
-
-            // Otherwise, it's a valid number.
-            validated.push_back(stoi(token));
-            token = "";
-
-        } else {
-            if (!isdigit(c)) {
-                throw runtime_error("The character '" + string(1, c) + "' was found in the input.");
-                return {};
-            }
-
-            // Leading zeros in numbers are ignored.
-            if (token.empty() && c == '0') continue;
-            token += c;
-        }
-    }
-
-    // The first token represents the number of packets,
-    // so if the number doesn't match, that's an error.
-    if ((!validated.empty() && validated.size() == validated[0] + 1) && (validated[0] != 0))
-        return validated;
-    else {
-        throw runtime_error("The number of packet module numbers does not match"
-                            " the number of packets (first number in the input),"
-                            " or the input is empty.");
-        return {};
-    }
+    return fileName;
 }
 
 string getFileName(){
@@ -105,7 +121,7 @@ string getFileName(){
     tm* localTime = localtime(&now);
 
     ostringstream oss;
-    oss << "generated_tests_";
+    oss << "generated_scenarios_";
 
     oss << (localTime->tm_year - 100)
         << setfill('0') << setw(2) << (localTime->tm_mon + 1)
